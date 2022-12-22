9c483f9529a3b715e5d3a47d3607900a017df91d
Chase <chase@macro.com>
2022-12-22 23:37:12 +0800

    Make macOS paths more consistent with other platforms

--- libreoffice-core/solenv/gbuild/platform/macosx.mk
+++ libreoffice-core/solenv/gbuild/platform/macosx.mk
@@ -373,7 +373,7 @@ endef
 
 # Python
 gb_Python_PRECOMMAND := PYTHONPATH="$$PYPATH"
-gb_Python_INSTALLED_EXECUTABLE := $(INSTROOT)/$(LIBO_LIB_FOLDER)/LibreOfficePython.framework/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/Resources/Python.app/Contents/MacOS/LibreOfficePython
+gb_Python_INSTALLED_EXECUTABLE := /bin/sh $(INSTROOT)/program/Python
 # this is passed to gdb as executable when running tests
 gb_Python_INSTALLED_EXECUTABLE_GDB := $(gb_Python_INSTALLED_EXECUTABLE)
 
