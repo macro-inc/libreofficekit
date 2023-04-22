c6a3aac7e3803d529e31c22ff8392ba1f223f1a7
Chase Colman <chase@macro.com>
2023-04-22 10:12:10 +0800

    Fix layout idling before render, wait for LOK init

--- libreoffice-core/sw/source/core/doc/DocumentTimerManager.cxx
+++ libreoffice-core/sw/source/core/doc/DocumentTimerManager.cxx
@@ -55,7 +55,6 @@ void DocumentTimerManager::StartIdling()
     if (m_bWaitForLokInit && comphelper::LibreOfficeKit::isActive())
     {
         // Start the idle jobs only after a certain delay.
-        m_bWaitForLokInit = false;
         StopIdling();
         return;
     }
