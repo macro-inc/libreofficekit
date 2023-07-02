c53962ca438ca5461807d1689e953e1a6343d0d5
Chase Colman <chase@macro.com>
2023-07-02 18:43:02 +0800

    Squashed 'libreoffice-core/' changes from 5fd62052499b..b95750af717e
    b95750af717e LOK: Navigator: fix expand problems
    a2ba85878bfb Update git submodules
    07f3199b06ef tdf#122676 sc: fix format with * (filling) and NBSP thousand separator
    738d7c7198a7 xmloff: ODF import: adapt generator parsing to 2-digit major versions
    285da02c047f tdf#155863 sd: fix resizing of cropped images to original size
    8b9913cab48b Update language-subtag-registry to 2023-05-11
    49076ef470fd tdf#153437 sc: fix broken formatting without performance regression
    f5f41aee008d Update pdfium to 5778
    85210161a54e Update pdfium to 5636
    082cb0dcf427 Update pdfium to 5522
    992c52bf049c tdf#153437 sc: fix broken formatting at Undo of row/column insertion
    298d635abf10 tdf#155324 sw: layout: try not to MoveFwd onto a page created by page break
    fd79d1169b60 tdf#155706 tdf#86630 sw page number wizard: make OK default
    43c8181b6555 upgrade libtiff to 4.5.1
    184b91d397e8 sc: prefix m_* ScViewFunctionSet class members
    28338b9bab0b Resolves: tdf#155750 allow private:factory urls in floating frames
    a1d9d0903380 tdf#156016 Fix bug where SVG check size would be 0
    4feaac8430f7 tdf#156050: Fix how misspelling line is calculated for RTL text
    f8e75b5c4511 Send writer document background color on theme change
    97436f1487f6 tdf#156078: Use TabOverSpacing compat option instead of TabOverflow
    021712f95009 Header columns can disappear with filtered data in pivot tables
    c9518e47f909 sc: filter: oox: fix crash delete ScDatabarFormatData
    a4195a77ca00 tdf#156078: also consider TabOverflow for tabs outside of line bounds
    a1acb19ec4c2 tdf#153466 PPTX import: fix "Custom position/size" background image
    d2d3d9e21630 tdf#153105 PPTX export: fix "Custom position/size" background image
    df482562d253 tdf#153107 OOXML export: fix scale of tile of shape background
    6460c685c3bc vcl: render scaled fonts for LOK
    b83c86b5474f LOK: send state of DataFilterAutoFilter
    6f7231d51a8f crashtesting: crash seen on exporting fdo66241-1.ods to xlsx
    cc6b8db56a76 Bump version to 23.05.1.2
    0759924b2e43 Revert "nss: upgrade to release 3.90"
    03c28615c1bb tdf#155685 sw: ExtendedSelectAll with tables, group the Undo objects
    280136fb683f tdf#155685 sw: fix another ExtendedSelectAll Redo crash w table at end
    c59ed7701605 tdf#107718: Fix script itemization of vertical text
    f923efb8e378 tdf#152048: Fix measuring text width with font fallback
    95a2b31b2245 tdf#151968: Fix vertical position of RTL spelling wavy line
    766e051f5069 ucb: webdav-curl: auth data is invalid when receiving 401
    2748f9c1a2e3 tdf#155685 sw: fix crash on undo of ExtendedSelectAll with table at end
    
    git-subtree-dir: libreoffice-core
    git-subtree-split: b95750af717e0693a13c3ef35277779e1394e0ae

--- svx/source/svdraw/svdotextdecomposition.cxx
+++ svx/source/svdraw/svdotextdecomposition.cxx
@@ -429,6 +429,12 @@ namespace
 
                         fStart = fTextWidth - fStart;
                         fEnd = fTextWidth - fEnd;
+
+                        // tdf#151968
+                        // if start < end, OutputDevice::DrawWaveLine() will
+                        // think it is a rotated line, so we swap fStart and
+                        // fEnd to avoid this.
+                        std::swap(fStart, fEnd);
                     }
 
                     // need to take FontScaling out of values; it's already part of
